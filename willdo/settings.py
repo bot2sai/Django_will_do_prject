95fb9c5adc37eb5e2eb3bf6d7346bf28d5dd2023
bot2sai <bot2sai@mail.ru>
2022-05-31 17:42:34 +0000

    5-th commit

--- willdo/settings.py
+++ willdo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w2g#-w)tmt&(yk2)qp6cn1_h%bb!l_v%+yijeso)=e%xy#ji+p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bot4sai.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT =  BASE_DIR / 'static'
 
 LOGIN_URL = '/login'
